7a526c652c6a804535b674a43e7a7fdaf2b8ec74
SangmeshMehter <ssmcmethre3080@gmail.com>
2024-11-13 02:02:06 +0530

    TL modified App.java

--- GitProject01/src/main/java/com/nt/service/App.java
+++ GitProject01/src/main/java/com/nt/service/App.java
@@ -8,6 +8,10 @@ public class App {
 	public int sum(int x, int y) {
 		return x+y;
 	}
+	
+	public int multi(int x, int y) {
+		return x*y;
+	}
     public static void main(String[] args) {
     	
     	App app = new App();
