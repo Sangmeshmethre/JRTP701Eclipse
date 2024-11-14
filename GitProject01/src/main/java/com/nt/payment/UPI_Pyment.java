a9f93cfaf6da08edd65500eea5296603bc941f08
SangmeshMehter <ssmcmethre3080@gmail.com>
2024-11-14 17:43:44 +0530

    Dev2 - created pyment branch and added upi_payment class

---
+++ GitProject01/src/main/java/com/nt/payment/UPI_Pyment.java
@@ -0,0 +1,9 @@
+package com.nt.payment;
+
+public class UPI_Pyment {
+
+	public String doPayment(long phoneno, double amount) {
+		System.out.println("Payment is done");
+		return "pyamet is done";
+	}
+}
