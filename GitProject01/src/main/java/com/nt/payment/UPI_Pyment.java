a61ddb685f15f73ae99a0f4f683ef0a7db330301
SangmeshMehter <ssmcmethre3080@gmail.com>
2024-11-14 17:52:52 +0530

    dev2- delete

--- GitProject01/src/main/java/com/nt/payment/UPI_Pyment.java
+++
@@ -1,9 +0,0 @@
-package com.nt.payment;
-
-public class UPI_Pyment {
-
-	public String doPayment(long phoneno, double amount) {
-		System.out.println("Payment is done");
-		return "pyamet is done";
-	}
-}
