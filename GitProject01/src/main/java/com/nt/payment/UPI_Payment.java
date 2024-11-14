6a487385692333e69a638bc20a022d6b21dfbed4
SangmeshMehter <ssmcmethre3080@gmail.com>
2024-11-14 18:04:26 +0530

    TL add paymetn method

---
+++ GitProject01/src/main/java/com/nt/payment/UPI_Payment.java
@@ -0,0 +1,9 @@
+package com.nt.payment;
+
+public class UPI_Payment {
+
+	public String doPayment(long accountno, double number) {
+		System.out.println("payment deltails");
+		return "Pyment is done";
+	}
+}
